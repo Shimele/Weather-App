1607b606f97bd58572f4b49392c6243221bcc71b
shimele <bechembayamba@gmail.com>
2020-09-23 09:52:00 +0100

    work in progress

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //selecting elements
 const searchButton = document.querySelector(".button");
-const cityInput = document.querySelector(".cityInput").value;
+
 const notifElement = document.querySelector(".notif");
-const iconElement = document.querySelector(".temp-icon");
+const iconElement = document.querySelector(".temp-icon img");
 const windElement = document.querySelector(".wind-val p");
 const descriptElement = document.querySelector(".temp-description p");
 const locationElement = document.querySelector(".temp-location p");
@@ -28,9 +28,10 @@ let openWeatherData = {};
 //xhr.send();
 
 searchButton.addEventListener("click", function weatherInfo() {
-  console.log(cityInput);
+  const cityInput = document.querySelector(".cityInput").value;
+
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=yaounde&appid=3bfbd0271d33f048e213c7535c45165f&units=metric`
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=3bfbd0271d33f048e213c7535c45165f&units=metric`
   )
     .then(function (response) {
       let data = response.json();
@@ -41,7 +42,8 @@ searchButton.addEventListener("click", function weatherInfo() {
       openWeatherData.description = data.weather[0].description;
       openWeatherData.temperature = Math.round(data.main.temp);
       const wind = Math.round(data.wind.speed);
-      const time = new Date(data.dt * 1000); //to display in miliseconds
+      const time = new Date(data.dt * 1000 - data.timezone * 1000); //to display in miliseconds
+      console.log(time);
       let hours = time.getHours();
       let minutes = time.getMinutes();
 
@@ -69,5 +71,25 @@ searchButton.addEventListener("click", function weatherInfo() {
       locationElement.textContent = cityAndTemp;
       descriptElement.textContent = openWeatherData.description;
       windElement.textContent = "WindSpeed:" + wind + "mph";
+
+      if (descriptElement.textContent == "thunderstorm with rain") {
+        iconElement.src = "/icons/thunderstorm.png";
+      } else if (descriptElement.textContent == "overcast clouds") {
+        iconElement.src = "/icons/overcast.png";
+      } else if (descriptElement.textContent == "broken clouds") {
+        iconElement.src = "/icons/broken.png";
+      } else if (descriptElement.textContent == "moderate rain") {
+        iconElement.src = "/icons/moderate.png";
+      } else if (descriptElement.textContent == "clear sky") {
+        iconElement.src = "/icons/clearSkies.png";
+      } else if (descriptElement.textContent == "light rain") {
+        iconElement.src = "/icons/light-rain.png";
+      } else if (descriptElement.textContent == "heavy intensity rain") {
+        iconElement.src = "/icons/heavy.png";
+      } else if (descriptElement.textContent == "few clouds") {
+        iconElement.src = "/icons/few-clouds.png";
+      } else if (descriptElement.textContent == "scattered clouds") {
+        iconElement.src = "/icons/scattered.png";
+      }
     });
 });
