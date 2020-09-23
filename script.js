a6262fbc992ccb99eef66f690d11b154621f5f00
shimele <bechembayamba@gmail.com>
2020-09-23 07:49:01 +0100

    work in progress

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //selecting elements
 const searchButton = document.querySelector(".button");
-const cityInput = document.querySelector(".cityInput");
+const cityInput = document.querySelector(".cityInput").value;
 const notifElement = document.querySelector(".notif");
 const iconElement = document.querySelector(".temp-icon");
 const windElement = document.querySelector(".wind-val p");
@@ -8,33 +8,66 @@ const descriptElement = document.querySelector(".temp-description p");
 const locationElement = document.querySelector(".temp-location p");
 
 let openWeatherData = {};
-let xhr = new XMLHttpRequest();
-xhr.open(
-  "GET",
-  "http://api.openweathermap.org/data/2.5/weather?q=yaounde&appid=3bfbd0271d33f048e213c7535c45165f&units=metric"
-);
-xhr.responseType = "text";
+//let xhr = new XMLHttpRequest();
+//xhr.open(
+//"GET",
+//"http://api.openweathermap.org/data/2.5/weather?q={cityInput}&appid=3bfbd0271d33f048e213c7535c45165f&units=metric"
+//);
+//xhr.responseType = "text";
 
-(xhr.onload = () => {
-  if (xhr.status === 200) {
-    notifElement.textContent = "Loading...";
-    openWeatherData = JSON.parse(xhr.responseText);
-    weatherInfo();
-  } else {
-    notifElement.textContent = "error :" + xhr.status;
-  }
-}),
-  xhr.send();
+//(xhr.onload = () => {
+//searchButton.addEventListener('submit', )
+//if (xhr.status === 200) {
+//notifElement.textContent = "Loading...";
+//openWeatherData = JSON.parse(xhr.responseText);
+//weatherInfo();
+//} else {
+//notifElement.textContent = "error :" + xhr.status;
+//}
+//}),
+//xhr.send();
 
-function weatherInfo() {
-  const location = openWeatherData.name;
-  const description = openWeatherData.weather[0].description;
-  const temperature = openWeatherData.main.temp;
-  const wind = Math.round(openWeatherData.wind.speed);
+searchButton.addEventListener("click", function weatherInfo() {
+  console.log(cityInput);
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=yaounde&appid=3bfbd0271d33f048e213c7535c45165f&units=metric`
+  )
+    .then(function (response) {
+      let data = response.json();
+      return data;
+    })
+    .then(function (data) {
+      openWeatherData.location = data.name;
+      openWeatherData.description = data.weather[0].description;
+      openWeatherData.temperature = Math.round(data.main.temp);
+      const wind = Math.round(data.wind.speed);
+      const time = new Date(data.dt * 1000); //to display in miliseconds
+      let hours = time.getHours();
+      let minutes = time.getMinutes();
 
-  const cityAndTemp = `${location} ${temperature}°C`;
+      //Format date and time display
+      let timeDisplay = "";
 
-  locationElement.textContent = cityAndTemp;
-  descriptElement.textContent = description;
-  windElement.textContent = "WindSpeed:" + wind + "mph";
-}
+      if (minutes < 10) {
+        //insert 0 infront of single digit e.g 05 instead of 5
+        minuts = `0${minutes}`;
+      }
+      //convert from military time (to english reading)
+      if (hours == 12) {
+        timeDisplay = `12:${minutes} PM`;
+      } else if (hours > 12) {
+        timeDisplay = `${hours - 12}:${minutes} PM`;
+      } else if (hours == 0) {
+        timeDisplay = `12:${minutes} AM`;
+      } else {
+        timeDisplay = `${hours}:${minutes} AM`;
+      }
+
+      //Populate Weather
+      const cityAndTemp = `${openWeatherData.location} ${openWeatherData.temperature}°C`;
+      notifElement.textContent = `Hi, it is ${timeDisplay}`;
+      locationElement.textContent = cityAndTemp;
+      descriptElement.textContent = openWeatherData.description;
+      windElement.textContent = "WindSpeed:" + wind + "mph";
+    });
+});
